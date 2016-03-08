c3ad0d3444acc5028bc5c304f92fbca577b66d7b
shirhxio <shirhxio537@gmail.com>
2016-03-08 15:25:38 +0900

    [sort]Organize images in Asset
    Assetフォルダのimages内を少し整理しました。
    Mainのタイマーの背景画像をUnityのPackageの物に差し替えました。

--- SQUARE_PUZZLE/Assets/Scripts/hand.cs
+++ SQUARE_PUZZLE/Assets/Scripts/hand.cs
@@ -1,25 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class hand : MonoBehaviour {
-	public float width = 0.6f;
-	private float period = 1.0f;
 
-	private Vector3 pos;
-	private float count = 0;
+public class hand : MonoBehaviour {
+	public float width = 0.6f;	//上下の動きの幅
+	public float period = 1.0f;	//上下の動きの周期(s)
+
+	private Vector2 offset = new Vector2 (0.1f, -0.6f);	//カーソルの指先をposの位置に持ってくるためのoffset
+	private Vector3 pos;		//カーソルの指し示す位置
+	private float count = 0;	//周期を制御するためのタイムカウンター
 
 	void Start () {
-		pos = transform.position;
+		pos = transform.position;	//初期状態ではposはtransform.positionとする
 	}
 
 	void Update () {
-		transform.position = new Vector3 (pos.x + 0.1f, pos.y - 0.6f + (width * Mathf.Sin(2 * Mathf.PI * count / period) / 2 - width / 2), 0);
+		//Y軸上を幅width、周期periodで上下運動(sin)する
+		transform.position = new Vector3 (pos.x + offset.x, pos.y + offset.y + (width * (Mathf.Sin(2 * Mathf.PI * count / period) -1) / 2), pos.z);
+
+		//タイムカウンターによる周期制御
+		count += Time.deltaTime;
 		if (count > period) {
 			count = 0;
 		}
-		count += Time.deltaTime;
 	}
 
+	//位置を変えるときはSetPos()を使いましょう。
 	public void SetPos(float x, float y){
 		pos.x = x;
 		pos.y = y;
